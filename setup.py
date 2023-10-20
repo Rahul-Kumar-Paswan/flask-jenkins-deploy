dd8e4e3a0455d9151f6d599136f49f3b15f46a20
Rahul-Kumar-Paswan <jekins@gmail.com>
2023-10-20 14:42:22 +0530

    cli: version updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flask_app",
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     description='A simple Python flask package',
     author='rahul@paswan',
